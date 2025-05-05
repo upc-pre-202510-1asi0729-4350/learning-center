bdf48986150ffba9098ddf8683a9b223c447244d
Angel Velasquez <pcisavel@upc.edu.pe>
2025-05-05 11:20:55 -0500

    feat(in-app-navigation): setup navigation routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './public/pages/home/home.component';
 
-export const routes: Routes = [];
+const AboutComponent =
+  () => import('./public/pages/about/about.component')
+    .then(m => m.AboutComponent);
+
+export const routes: Routes = [
+  { path: 'home',   component:      HomeComponent },
+  { path: 'about',  loadComponent:  AboutComponent },
+  { path: '',       redirectTo:     'home', pathMatch: 'full' }
+];
