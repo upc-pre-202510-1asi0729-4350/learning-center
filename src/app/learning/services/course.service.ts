ec52bd8007662bbc689d9befe42ea4d8682eb178
Angel Velasquez <pcisavel@upc.edu.pe>
2025-05-05 12:51:24 -0500

    feat(courses): add course service.

---
+++ src/app/learning/services/course.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from '../../shared/services/base.service';
+import {Course} from '../model/course.entity';
+import {environment} from '../../../environments/environment';
+
+const coursesResourceEndpointPath = environment.coursesEndpointPath;
+
+@Injectable({providedIn: 'root'})
+export class CourseService extends BaseService<Course>{
+
+  constructor() {
+    super();
+    this.resourceEndpoint = coursesResourceEndpointPath;
+  }
+}
