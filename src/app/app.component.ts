03f1f80b34e6b78ddee1e08aa3dbad451d57e741
Angel Velasquez <pcisavel@upc.edu.pe>
2025-05-05 12:01:28 -0500

    feat(i18n): add language switcher component to main view.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
 import {MatAnchor} from '@angular/material/button';
+import {LanguageSwitcherComponent} from './public/components/language-switcher/language-switcher.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar, MatToolbarRow, MatAnchor, RouterLink, RouterLinkActive, TranslatePipe],
+  imports: [RouterOutlet, MatToolbar, MatToolbarRow, MatAnchor, RouterLink, RouterLinkActive, TranslatePipe, LanguageSwitcherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/public/components/language-switcher/language-switcher.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
+
+@Component({
+  selector: 'app-language-switcher',
+  imports: [
+    MatButtonToggleGroup,
+    MatButtonToggle,
+  ],
+  templateUrl: './language-switcher.component.html',
+  styleUrl: './language-switcher.component.css'
+})
+export class LanguageSwitcherComponent {
+  protected currentLang: string = 'en';
+  protected languages: string[] = [ 'en', 'es' ]
+
+  constructor(private translate: TranslateService) {
+    this.currentLang = translate.currentLang;
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+    this.currentLang = language;
+  }
+}
