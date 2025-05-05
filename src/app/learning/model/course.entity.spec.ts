d23830c0288835119641e1a54dd6973b64f39a1e
Angel Velasquez <pcisavel@upc.edu.pe>
2025-05-05 12:31:13 -0500

    feat(courses): add course entity.

---
+++ src/app/learning/model/course.entity.spec.ts
@@ -0,0 +1,7 @@
+import { CourseEntity } from './course.entity';
+
+describe('CourseEntity', () => {
+  it('should create an instance', () => {
+    expect(new CourseEntity()).toBeTruthy();
+  });
+});
