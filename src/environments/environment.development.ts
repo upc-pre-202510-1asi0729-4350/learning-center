22c1c200ee09998fad5029bc628f43882100b97b
Angel Velasquez <pcisavel@upc.edu.pe>
2025-05-05 12:08:40 -0500

    feat(environments): environment variables.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  serverBaseUrl: 'http://localhost:3000/api/v1',
+  coursesEndpointPath: '/courses',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  serverBaseUrl: 'http://localhost:3000/api/v1',
+  coursesEndpointPath: '/courses',
+};
