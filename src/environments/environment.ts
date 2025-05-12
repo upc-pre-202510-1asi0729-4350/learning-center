9b9e5ccd68a03124eb3d803915c0e6fa7da32beb
Angel Velasquez <pcisavel@upc.edu.pe>
2025-05-12 12:57:15 -0500

    chore: add deployment configuration.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverBaseUrl: 'http://localhost:3000/api/v1',
+  serverBaseUrl: 'https://learning-center.free.beeceptor.com/api/v1',
   coursesEndpointPath: '/courses',
 };
