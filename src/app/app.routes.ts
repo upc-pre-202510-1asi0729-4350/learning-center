0b715257bafcc77e502c43c3b5cbc81cac3d4f2a
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-30 12:39:20 -0500

    feat(iam): add routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './public/pages/home/home.component';
+import {SignUpComponent} from './iam/pages/sign-up/sign-up.component';
+import {SignInComponent} from './iam/pages/sign-in/sign-in.component';
+import {authenticationGuard} from './iam/services/authentication.guard';
 
 const AboutComponent =
   () => import('./public/pages/about/about.component')
@@ -16,7 +19,9 @@ const CourseManagementComponent =
 export const routes: Routes = [
   { path: 'home',             component:      HomeComponent },
   { path: 'about',            loadComponent:  AboutComponent },
-  { path: 'learning/courses', loadComponent:  CourseManagementComponent},
+  { path: 'learning/courses', loadComponent:  CourseManagementComponent, canActivate: [authenticationGuard]},
+  { path: 'sign-in',          component:      SignInComponent },
+  { path: 'sign-up',          component:      SignUpComponent },
   { path: '',                 redirectTo:     'home', pathMatch: 'full' },
   { path: '**',               loadComponent:  PageNotFoundComponent }
 ];
