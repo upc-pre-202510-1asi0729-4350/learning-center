cd9bfaec0de78f35be4b3459026797f6cf3d990e
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-30 12:36:37 -0500

    chore: update local backend url.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  serverBaseUrl: 'http://localhost:3000/api/v1',
+  serverBaseUrl: 'http://localhost:8080/api/v1',
+  originalServerBaseUrl: 'http://localhost:3000/api/v1',
   coursesEndpointPath: '/courses',
 };
